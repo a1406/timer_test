56aac32af5b28e7a70434f2d75f66fcb13ef8b1e
jack <jack@arch.com>
2019-10-04 10:53:09 +0800

    1

--- test.c
+++ test.c
@@ -1,7 +1,13 @@
+#include <stdint.h>
 #include "./skynet_timer.h"
 
 int main(int argc, char *argv[])
 {
-    
-    return 0;
+	skynet_timer_init();
+	for (uint32_t i = 0; i < 3; ++i)
+	{
+		uint32_t start = 1 << (i * 10);
+		uint32_t end = i << (i * 10 + 12);
+	}
+	return 0;
 }
